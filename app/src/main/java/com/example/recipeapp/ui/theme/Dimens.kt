120a1b3eee9ecb01d42e8cd6be199e53fbaf147f
Svirydovich <standup.minsk@gmail.com>
2026-06-11 19:31:08 +0300

    RCA-06 Create and apply a color scheme

---
+++ app/src/main/java/com/example/recipeapp/ui/theme/Dimens.kt
@@ -0,0 +1,66 @@
+package com.example.recipeapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    object Spacing {
+        val XSmall = 4.dp
+        val Small = 8.dp
+        val Medium = 16.dp
+        val Large = 24.dp
+        val XLarge = 32.dp
+
+        val ScreenHorizontalSmall = 16.dp
+        val ScreenHorizontalMedium = 24.dp
+    }
+
+    object Heights {
+        val AppBar = 56.dp
+        val NavigationBar = 80.dp
+
+        val CardSmall = 140.dp
+        val CardMedium = 160.dp
+        val CardLarge = 200.dp
+
+        val DialogMinHeight = 280.dp
+    }
+
+    object Elevation {
+        val Level0 = 0.dp
+        val Level1 = 2.dp
+        val Level2 = 4.dp
+        val Level3 = 8.dp
+        val Level4 = 16.dp
+    }
+
+    object ElementSize {
+        val IconSmall = 18.dp
+        val IconMedium = 24.dp
+        val IconLarge = 36.dp
+
+        val AvatarSmall = 32.dp
+        val AvatarMedium = 48.dp
+        val AvatarLarge = 64.dp
+
+        val ProgressIndicatorSmall = 24.dp
+    }
+
+    object Button {
+        val MinHeight = 48.dp
+
+        val ContentPaddingHorizontalSmall = 12.dp
+        val ContentPaddingHorizontalMedium = 16.dp
+
+        val IconSize = ElementSize.IconMedium
+    }
+
+    object Slider {
+        val HeightWithLabels = 80.dp
+
+        val TrackHeight = 4.dp
+
+        val ThumbRadius = 12.dp
+
+        val ThumbPressedRadius = 16.dp
+    }
+}
