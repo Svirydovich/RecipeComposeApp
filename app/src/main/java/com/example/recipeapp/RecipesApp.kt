b5dbdffba22bf097379f2fa5b7aa2e379440425a
Svirydovich <standup.minsk@gmail.com>
2026-06-24 22:59:43 +0300

    RCA-08: Create and configure basic navigation

--- app/src/main/java/com/example/recipeapp/RecipesApp.kt
+++ app/src/main/java/com/example/recipeapp/RecipesApp.kt
@@ -1,20 +1,50 @@
 package com.example.recipeapp
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.recipeapp.ui.theme.RecipeAppTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import com.example.recipeapp.core.ui.navigation.BottomNavigation
 
 @Composable
 fun RecipesApp() {
     RecipeAppTheme {
-        Scaffold { paddingValues ->
-            Text(
-                "Recipes App",
-                modifier = Modifier.padding(paddingValues)
-            )
+        var currentScreen by remember { mutableStateOf(ScreenId.CATEGORIES) }
+        Scaffold(
+            bottomBar = {
+                BottomNavigation(
+                    onCategoriesClick = { currentScreen = ScreenId.CATEGORIES },
+                    onFavoriteClick = { currentScreen = ScreenId.FAVORITES }
+                )
+            }
+        ) { paddingValues ->
+            when (currentScreen) {
+                ScreenId.CATEGORIES -> {
+                    Box(
+                        modifier = Modifier.padding(paddingValues),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Категории")
+                    }
+                }
+
+                ScreenId.FAVORITES -> {
+                    Box(
+                        modifier = Modifier.padding(paddingValues),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Избранное")
+                    }
+                }
+            }
         }
     }
 }
