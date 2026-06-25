ffb220d59c150ffca7452a5a29ccee64c7c460bb
Svirydovich <standup.minsk@gmail.com>
2026-06-26 02:18:43 +0300

    RCA-09 Start implementing the "Categories" screen

---
+++ app/src/main/java/com/example/recipeapp/ui/components/ScreenHeader.kt
@@ -0,0 +1,37 @@
+package com.example.recipeapp.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import com.example.recipeapp.ui.theme.Dimens
+
+@Composable
+fun ScreenHeader(
+    imagePainter: Painter,
+    contentDescription: String,
+    title: String
+) {
+    Box(
+        modifier = Modifier.height(Dimens.Heights.HeaderHeight)
+    ) {
+        Image(
+            painter = imagePainter,
+            contentDescription = contentDescription,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Surface(
+            modifier = Modifier.align(Alignment.BottomStart)
+        ) {
+            Text(text = title)
+        }
+    }
+}
