ecc25fa754b40854902f222d617c6451070ca1cb
Svirydovich <standup.minsk@gmail.com>
2026-08-03 01:19:00 +0300

    RCA-17 Passing Parcelable objects between screens

--- app/src/main/java/com/example/recipeapp/ui/recipes/model/IngredientUiModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/model/IngredientUiModel.kt
@@ -1,14 +1,17 @@
 package com.example.recipeapp.ui.recipes.model
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
 import com.example.recipeapp.data.model.IngredientDto
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Immutable
 data class IngredientUiModel(
     val name: String,
     val quantity: String,
     val unitOfMeasure: String
-)
+) : Parcelable
 
 fun IngredientDto.toUiModel() = IngredientUiModel(
     name = description,
