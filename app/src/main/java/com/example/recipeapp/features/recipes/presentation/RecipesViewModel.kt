f66fd8557fa1ab48224caa5c2a0998d5fe7cea6d
Svirydovich <standup.minsk@gmail.com>
2026-08-23 00:13:10 +0300

    RCA-27 Update code after the second review

--- app/src/main/java/com/example/recipeapp/features/recipes/presentation/RecipesViewModel.kt
+++ app/src/main/java/com/example/recipeapp/features/recipes/presentation/RecipesViewModel.kt
@@ -9,7 +9,6 @@ import com.example.recipeapp.data.repository.RecipesRepository
 import com.example.recipeapp.features.recipes.presentation.model.RecipesUiState
 import com.example.recipeapp.features.recipes.presentation.model.toUiModel
 import com.example.recipeapp.navigation.Destination
-import com.example.recipeapp.navigation.Destination.Companion.CATEGORY_IMAGE_ARG
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -18,29 +17,31 @@ import kotlinx.coroutines.launch
 import kotlin.coroutines.cancellation.CancellationException
 
 class RecipesViewModel(
-    private val savedStateHandle: SavedStateHandle,
+    savedStateHandle: SavedStateHandle,
     private val repository: RecipesRepository = RecipesRepository()
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(RecipesUiState(isLoading = true))
     val uiState: StateFlow<RecipesUiState> = _uiState.asStateFlow()
-    val categoryImageUrl: String = Uri.decode(savedStateHandle[CATEGORY_IMAGE_ARG] ?: "")
-        .let { if (it.startsWith("http")) it else "${Constants.ASSETS_URI_PREFIX}$it" }
 
     init {
-        val rawImageUrl: String? = savedStateHandle[Destination.CATEGORY_IMAGE_ARG]
-
-        val decodedTitle =
-            Uri.decode(savedStateHandle[Destination.CATEGORY_TITLE_ARG] ?: "")
-        val decodedImageUrl = Uri.decode(rawImageUrl).orEmpty()
-
         val categoryId: Int = requireNotNull(savedStateHandle[Destination.CATEGORY_ID_ARG]) {
             "Ошибка навигации: ${Destination.CATEGORY_ID_ARG} требуется для экрана рецептов"
         }
+        val decodedTitle =
+            Uri.decode(savedStateHandle[Destination.CATEGORY_TITLE_ARG] ?: "")
+        val decodedImageUrl = Uri.decode(savedStateHandle[Destination.CATEGORY_IMAGE_ARG] ?: "")
+
+        _uiState.update { current ->
+            current.copy(
+                categoryTitle = decodedTitle,
+                categoryImageUrl = decodedImageUrl.let {
+                    if (it.startsWith("http")) it else "${Constants.ASSETS_URI_PREFIX}$it"
+                }
+            )
+        }
 
         loadRecipes(categoryId)
-
-        updateHeader(decodedTitle, decodedImageUrl)
     }
 
     private fun loadRecipes(categoryId: Int) {
@@ -64,15 +65,6 @@ class RecipesViewModel(
                     error = e.localizedMessage ?: "Неизвестная ошибка"
                 )
             }
-        }
-    }
-
-    private fun updateHeader(title: String, imageUrl: String) {
-        _uiState.update { current ->
-            current.copy(
-                categoryTitle = title,
-                categoryImageUrl = imageUrl
-            )
         }
     }
 }
