731e19af90175f5a64a8b3627396fc474068b784
Svirydovich <standup.minsk@gmail.com>
2026-08-28 23:35:21 +0300

    RCA-31 The first network request (URLConnection)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -63,4 +64,5 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recipeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,7 +11,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import com.example.recipeapp.data.model.CategoryDto
 import com.example.recipeapp.ui.theme.RecipeAppTheme
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlinx.serialization.json.Json
 
 class MainActivity : ComponentActivity() {
     private var deepLinkIntent by mutableStateOf<Intent?>(null)
@@ -26,6 +34,52 @@ class MainActivity : ComponentActivity() {
         setContent {
             RecipesApp(deepLinkIntent = deepLinkIntent)
         }
+
+        Log.i(
+            "MainActivity",
+            "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}"
+        )
+
+        val thread = Thread {
+            Log.i("MainActivity", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+
+            try {
+                val url = URL("https://recipes.androidsprint.ru/api/category")
+                val connection = url.openConnection() as HttpURLConnection
+
+                try {
+                    connection.connect()
+
+                    val code = connection.responseCode
+
+                    if (code in 200..299) {
+                        val jsonString = connection.inputStream.bufferedReader().readText()
+
+                        Log.i("MainActivity", "responseCode: ${connection.responseCode}")
+                        Log.i("MainActivity", "responseMessage: ${connection.responseMessage}")
+                        Log.i("MainActivity", "Body: $jsonString")
+
+                        val categories = Json.decodeFromString<List<CategoryDto>>(jsonString)
+                        Log.i(
+                            "MainActivity",
+                            "Получено категорий: ${categories.size}: ${categories.joinToString(", ") { it.title }}"
+                        )
+                    } else {
+                        connection.errorStream?.bufferedReader()?.readText() ?: ""
+                        Log.i(
+                            "MainActivity",
+                            "Ошибка HTTP. Код: $code. Сообщение: ${connection.responseMessage}"
+                        )
+                    }
+                } finally {
+                    connection.disconnect()
+                }
+            } catch (e: Exception) {
+                Log.e("MainActivity", "Ошибка при выполнении запроса", e)
+            }
+        }
+
+        thread.start()
     }
 
     override fun onNewIntent(intent: Intent) {
