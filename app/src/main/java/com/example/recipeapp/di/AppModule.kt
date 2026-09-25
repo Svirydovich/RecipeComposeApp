37977b289e045f7fe864ad3e8fcb54f3f662a235
Svirydovich <standup.minsk@gmail.com>
2026-09-25 17:58:41 +0300

    RCA-46 Update code after review

--- app/src/main/java/com/example/recipeapp/di/AppModule.kt
+++ app/src/main/java/com/example/recipeapp/di/AppModule.kt
@@ -6,24 +6,28 @@ import com.example.recipeapp.core.network.api.RecipesApiService
 import com.example.recipeapp.data.database.RecipesDatabase
 import com.example.recipeapp.data.repository.RecipesRepository
 import com.example.recipeapp.data.repository.RecipesRepositoryImpl
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
+    private val json = Json { ignoreUnknownKeys = true; coerceInputValues = true }
     @Provides
     @Singleton
     fun provideRecipesApiService(): RecipesApiService {
         return Retrofit.Builder()
             .baseUrl(NetworkConfig.BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
             .create(RecipesApiService::class.java)
     }
